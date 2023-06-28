d7dfd6727e3a6cff9bc421bdd196b092b49e7de5
lindxdeep <linder.igor@gmail.com>
2023-06-28 20:10:00 +0300

    Треугольник серпинского

--- Program.cs
+++ Program.cs
@@ -1,19 +1,5 @@
-﻿Console.Clear();
-// Console.SetCursorPosition(10, 4);
-// Console.WriteLine("*");
-
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
-    xc = 80, yc = 30;
-
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("*");
-
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("*");
-
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("*");
+﻿Console.Write("Enter name: ");
+string username = Console.ReadLine();
 
 int x = xa, y = xb;
 int count = 0;
