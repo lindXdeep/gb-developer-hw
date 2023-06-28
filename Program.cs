997cf6d1da9e66e5c186114fe0421b61c331b953
lindxdeep <linder.igor@gmail.com>
2023-06-29 01:16:07 +0300

    Задача:2-какое число большее, а какое меньшее

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+  public static void Main(String[] args)
+  {
+    string line = Console.ReadLine();
+    string[] strSplit = line.Split(' ');
+
+    int a = Convert.ToInt32(strSplit[0]);
+    int b = Convert.ToInt32(strSplit[1]);
+
+    int max = a < b ? b : a;
+    int min = a > b ? b : a;
+
+    Console.WriteLine($"min = {min}, max = {max}");
+  }
+}
