0366be01d62d3a37db8f7a43876dc6fc213b5d36
lindxdeep <linder.igor@gmail.com>
2023-06-29 22:53:17 +0300

    является ли число палиндромом

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int t = n;
+    int p = 0;
+    while (n > 0)
+    {
+      int x = n % 10;
+
+      n /= 10;
+
+      p = p * 10 + x;
+    }
+
+    Console.WriteLine(t == p ? "да" : "нет");
+  }
+}
