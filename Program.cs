fabf8217020c2f5e723c96849b3621ad01b0af58
lindxdeep <linder.igor@gmail.com>
2023-07-18 14:02:59 +0300

    Задача 52. Найдите среднее арифметическое в каждом столбце.

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ class Program
 {
   public static void Main(string[] args)
   {
-    int m = 3;
-    int n = 4;
+    int m = 2;
+    int n = 3;
 
     int[,] arr = new int[m, n];
 
     for (int i = 0; i < m; i++)
       for (int j = 0; j < n; j++)
-        arr[i, j] = Math.Ceiling(new Random().NextDouble() * 20);
+        arr[i, j] = (int)(new Random().NextDouble() * 20);
 
     int rows = arr.GetUpperBound(0) + 1;
     int cols = arr.Length / rows;
@@ -19,9 +19,23 @@ class Program
     for (int i = 0; i < rows; i++)
     {
       for (int j = 0; j < cols; j++)
-        Console.Write("{0, 7}", arr[i, j]);
+        Console.Write("{0, 5}", arr[i, j]);
       Console.WriteLine();
     }
 
+    double[] count_cols = new double[rows + 1];
+
+    for (int j = 0; j < cols; j++)
+    {
+      double sum = 0;
+      for (int i = 0; i < rows; i++)
+        sum += arr[i, j];
+      count_cols[j] = sum / rows;
+    }
+
+    Console.WriteLine();
+    foreach (var s in count_cols)
+      Console.Write("{0, 5} ", s);
+    Console.WriteLine();
   }
 }
