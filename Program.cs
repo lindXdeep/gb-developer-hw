17aa3b7bf04be1531d8d4b97be0e93024475b35d
lindxdeep <linder.igor@gmail.com>
2023-07-20 11:29:47 +0300

    Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.

--- Program.cs
+++ Program.cs
@@ -4,53 +4,23 @@ class Program
 {
   public static void Main(string[] args)
   {
-    int[,] A = {
-      {1,2},
-      {3,4}
-    };
+    Random r = new Random();
 
-    int[,] B = {
-      {5,6,7},
-      {8,9,10}
-    };
+    int M = 2, N = 2, K = 2;
 
-    int rowA = A.GetUpperBound(0) + 1;
-    int colA = A.Length / rowA;
+    int[,,] arr = new int[M, N, K];
 
-    int rowB = B.GetUpperBound(0) + 1;
-    int colB = B.Length / rowB;
-
-    int[,] C = new int[rowB, colB];
-
-    if (colA == rowB)
+    for (int i = 0; i < M; i++)
     {
-      for (int i = 0; i < rowB; i++)
-        for (int l = 0; l < colA; l++)
-          for (int j = 0; j < colB; j++)
-            C[l, j] += A[l, i] * B[i, j];
-    }
-    else
-    {
-      Console.WriteLine($"Такую матрицу перемножать нельзя");
-    }
-
-    printArray(A);
-    printArray(B);
-    printArray(C);
-  }
-
-  private static void printArray(int[,] arr)
-  {
-    Console.WriteLine();
-
-    int row = arr.GetUpperBound(0) + 1;
-    int col = arr.Length / row;
-
-    for (int i = 0; i < row; i++)
-    {
-      for (int j = 0; j < col; j++)
-        Console.Write($"{arr[i, j]} ");
-      Console.WriteLine();
+      for (int j = 0; j < N; j++)
+      {
+        for (int l = 0; l < K; l++)
+        {
+          arr[i, j, l] = r.Next(10, 99);
+          Console.Write($"{arr[i, j, l]}({i},{j},{l}) ");
+        }
+        Console.WriteLine();
+      }
     }
   }
 }
