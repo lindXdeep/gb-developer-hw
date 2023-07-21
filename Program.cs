f001d96684f76d5c72f7862943ee38d2c2cc3fbb
lindxdeep <linder.igor@gmail.com>
2023-07-21 12:08:21 +0300

    Задача 62: Напишите программу, которая заполнит спирально массив

--- Program.cs
+++ Program.cs
@@ -4,23 +4,55 @@ class Program
 {
   public static void Main(string[] args)
   {
-    Random r = new Random();
+    int M = 5, N = 10;
+    int[,] arr = new int[M, N];
 
-    int M = 2, N = 2, K = 2;
+    int i = 0;
+    int j = -1;
 
-    int[,,] arr = new int[M, N, K];
+    int row = 0;
+    int col = 1; // начинаем движение в право
 
-    for (int i = 0; i < M; i++)
+    int t = 1;
+    while (t <= M * N)
     {
-      for (int j = 0; j < N; j++)
+      if (
+            (i + row >= 0 & i + row < M) &&
+            (j + col >= 0 & j + col < N) &&
+            arr[i + row, j + col] == 0
+         )
       {
-        for (int l = 0; l < K; l++)
-        {
-          arr[i, j, l] = r.Next(10, 99);
-          Console.Write($"{arr[i, j, l]}({i},{j},{l}) ");
-        }
-        Console.WriteLine();
+        i += row;
+        j += col;
+        arr[i, j] = t++;
       }
+      else if (col == 1)
+      {
+        row = 1;
+        col = 0;
+      }
+      else if (row == 1)
+      {
+        col = -1;
+        row = 0;
+      }
+      else if (col == -1)
+      {
+        col = 0;
+        row = -1;
+      }
+      else if (row == -1)
+      {
+        row = 0;
+        col = 1;
+      }
+    }
+
+    for (i = 0; i < M; i++)
+    {
+      for (j = 0; j < N; j++)
+        Console.Write("{0,4}", arr[i, j]);
+      Console.WriteLine();
     }
   }
 }
