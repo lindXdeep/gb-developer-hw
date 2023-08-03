fcc4cfd36c315c55e25f97f3f394f6d70eeef141
lindxdeep <linder.igor@gmail.com>
2023-08-03 17:50:22 +0300

    add method refernce and testing method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 
+delegate string[] Operation(string[] lines);
 class Program
 {
   public static void Main(string[] args)
@@ -9,12 +10,20 @@ class Program
       new string[] { "1234", "1567", "-2", "computer science" },
       new string[] { "Russia", ":Denmark", "Kazan" }
     };
+
+    TestGetLines(lines, GetLines);
   }
-  
+
+  public static void TestGetLines(string[][] strings, Operation op)
+  {
+    string[] s = op(strings[0]);
+
+    Console.WriteLine("Testing: " + s[0]);
+  }
 
   public static string[] GetLines(string[] lines)
   {
-
     return lines;
   }
 }
+
