88eba9a4de10f259a3db66159370b00b7a177690
lindxdeep <linder.igor@gmail.com>
2023-08-03 16:28:23 +0300

    add arrays for test function

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    string[][] lines = {
+      new String[] { "Hello", "2", "world", ":-)"},
+      new string[] { "1234", "1567", "-2", "computer science" },
+      new string[] { "Russia", ":Denmark", "Kazan" }
+    };
+  }
+  
+
+  public static string[] GetLines(string[] lines)
+  {
+
+    return lines;
+  }
+}
