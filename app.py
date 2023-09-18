a5cbf9be0a2073171c93ca28188ed6cc744a753b
lindxdeep <linder.igor@gmail.com>
2023-09-18 14:31:23 +0300

    Задача 6

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-i = int(input())
-j = int(input())
+n = 385916
 
-print(i + j -1)
+x = str(n)
+s = int(len(x) / 2)
+
+l = x[:s]
+r = x[s:]
+
+a = 0
+for i in l:
+  a += int(i)
+
+b = 0
+for i in r:
+  b += int(i)
+
+print(('no','yes')[a == b])
