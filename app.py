d1a25df4014951530ffa451c13c8bb367d3e4d7c
lindxdeep <linder.igor@gmail.com>
2023-09-19 12:11:27 +0300

    Задача 14

--- app.py
+++ app.py
@@ -1,24 +1,6 @@
-import random
-
 n = int(input())
 
-l = list()
-i = n
-while i > 0:
-  l.append(random.randint(-50, 50))
-  i -= 1
-
-max = 0
-cnt = 0
-i = 0
+i = 1
 while i < n:
-  if l[i] > 0:
-    cnt += 1
-  else:
-    if cnt > max:
-      max = cnt
-    cnt = 0
-  
-  i += 1
-
-print(f"{n} -> {l} \n{max}")
+  print(i)
+  i *= 2
