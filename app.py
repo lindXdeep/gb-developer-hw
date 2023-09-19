819e513e17c5b9fd3f78bb5410bb57b4b3c15516
lindxdeep <linder.igor@gmail.com>
2023-09-19 09:47:35 +0300

    Задача 12

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-a = 21
+s, p = map(int, input().split())
 
-n = 0
+y = 0
 
-f1 = 1
-f2 = 1
-while f1 < a - 1 > 0:
-  f1, f2 = f2, f1 + f2
-  a -= 1 
-  n += 1
+i = 1
+while i < s:
+  if p % i == 0:
+    y = i
+  i += 1
 
-print(n + 2)
+x = s - y
 
+print(x, y)
