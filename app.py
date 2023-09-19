ceae15f1023502192d0cee3e83ce6b2279f38b7e
lindxdeep <linder.igor@gmail.com>
2023-09-19 12:02:28 +0300

    Задача 13

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-s, p = map(int, input().split())
+import random
 
-y = 0
+n = int(input())
 
-i = 1
-while i < s:
-  if p % i == 0:
-    y = i
+l = list()
+i = n
+while i > 0:
+  l.append(random.randint(-50, 50))
+  i -= 1
+
+max = 0
+cnt = 0
+i = 0
+while i < n:
+  if l[i] > 0:
+    cnt += 1
+  else:
+    if cnt > max:
+      max = cnt
+    cnt = 0
+  
   i += 1
 
-x = s - y
-
-print(x, y)
+print(f"{n} -> {l} \n{max}")
