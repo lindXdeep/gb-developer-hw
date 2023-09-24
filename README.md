e06c2323b6eff83a4e94eb4e0f617258b7ac53ea
lindxdeep <linder.igor@gmail.com>
2023-09-24 18:06:49 +0300

    Задача 21

--- README.md
+++ README.md
@@ -233,7 +233,14 @@ Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 ### Решение:
 
 ```python
+dic = {"V": "S001", "V": "S002", "VI": "S001", "VI": "S005", "VII": "S005", "V":"S009", "VIII ":"S007"}
 
+dic_unique = {}
+
+for i in dic:
+  if i not in dic_unique:
+    dic_unique[i] = dic[i]
+    print(dic_unique[i])
 ```
 
 ## Задача 23:
--- app.py
+++ app.py
@@ -1,29 +1,9 @@
-one = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
-two = ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У']
-three = ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я']
-fore = ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы']
-five = ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч']
-eight = ['J', 'X','Ш', 'Э', 'Ю']
-ten = ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+dic = {"V": "S001", "V": "S002", "VI": "S001", "VI": "S005", "VII": "S005", "V":"S009", "VIII ":"S007"}
 
-k = 'ноутбук'
+dic_unique = {}
 
-cost = 0
+for i in dic:
+  if i not in dic_unique:
+    dic_unique[i] = dic[i]
+    print(dic_unique[i])
 
-for i in k:
-  if i.capitalize() in one:
-    cost += 1    
-  elif i.capitalize() in two:
-    cost += 2
-  elif i.capitalize() in three:
-    cost += 3
-  elif i.capitalize() in fore:
-    cost += 4
-  elif i.capitalize() in five:
-    cost += 5
-  elif i.capitalize() in eight:
-    cost += 8
-  elif i.capitalize() in ten:
-    cost += 10
-
-print(cost)
