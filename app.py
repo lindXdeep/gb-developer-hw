7fb9ee62bf036ac2f1f2e57b4303d343ba6d0ad1
lindxdeep <linder.igor@gmail.com>
2023-09-24 17:07:03 +0300

    Задача 19

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-list_1 = [2, 4, 1, 6, 8, 2, 9, 3, 2, 5]
-k = 10
+list = [1, 2, 3, 4, 5]
+k = 3
 
-a = list_1[0]
-b = list_1[len(list_1)-1]
+for i in range(k - 1):
+  list.insert(0, list.pop())
 
-for i in list_1:
-  if k >= i > a:
-    a = i
-  elif k < i < b:
-    b = i 
-
-print((a, b)[abs(k - a) > abs(b - k)])
-
-
-
+  print(list)
