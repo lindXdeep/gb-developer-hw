17cb5c8bd76edbbdce409b4dd033787dd0c97912
lindxdeep <linder.igor@gmail.com>
2023-09-26 18:59:17 +0300

    Задача 27: Количество уникальных слов в этом тексте

--- README.md
+++ README.md
@@ -161,7 +161,11 @@ print(*list)
 ### Решение:
 
 ```python
+s = 'She sells sea shells on the sea shore The shells that she sells are sea shells I\'m sure.So if she sells sea shells on the sea shore I\'m sure that the shells are sea shore shells'
+s = s.lower().split()
+s = set(s)
 
+print(len(s))
 ```
 
 ## Задача 29:
--- app.py
+++ app.py
@@ -1,20 +1,5 @@
-list = ['a', 'a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 'd', 'd']
+s = 'She sells sea shells on the sea shore The shells that she sells are sea shells I\'m sure.So if she sells sea shells on the sea shore I\'m sure that the shells are sea shore shells'
+s = s.lower().split()
+s = set(s)
 
-s = len(list)
-
-i = 0
-while i < s:
-  e = list[i]
-  
-  if e.find('_') == -1:
-    c = 0
-    j = i + 1
-    if e in list[j:]:
-      while j < s:
-        if list[j] == e:
-          c += 1
-          list[j] = list[i] + '_' + str(c)
-        j += 1
-  i += 1 
-
-print(*list)
+print(len(s))
