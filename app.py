9766db5ddf4dfe24cd35c4eb52becb2ab3d7354c
lindxdeep <linder.igor@gmail.com>
2023-09-26 11:52:07 +0300

    Задача 23: Подсчитайть количество элементов, превышающих предыдущий элемент

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-n, m = map(int, input().split())
+list = [0, -1, 5, 2, 3]
 
-list_n = list(map(int, input().split()))[:n]
-list_m = list(map(int, input().split()))[:m]
-
+count = 0
 i = 0
-while i < n - 1:
-  if list_n[i] in list_n[i + 1:]:
-    list_n.pop(i)
-    n -= 1
-  else:
-    i += 1
+while i < len(list) - 1:
+  if list[i] < list[i + 1]:
+    count += 1
+  i += 1
 
-i = 0
-while i < m:
-  if list_m[i] not in list_n:
-    list_m.pop(i)
-    m -= 1
-  else:
-    i += 1
+print(count)
 
-print(*sorted(list_m))
+  
