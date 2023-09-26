3fcdb3d1a734885e3c66d682a13e610251d3f5db
lindxdeep <linder.igor@gmail.com>
2023-09-26 18:34:28 +0300

    Задача 25: Найти количество повторений

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-list = input().split()
-size = len(list)
+list = ['a', 'a', 'a', 'b', 'c', 'a', 'a', 'd', 'c', 'd', 'd']
 
-i = 0 
-while i < size:
-  if list[i] in list[i + 1:]:
-    j = list[i + 1:].index(list[i])
-    print("::", j)
-    while j < size:
-      print(list)
-      list[j] = list[i] + '_' + str(1)
-      j = list[j + 1:].index(list[i])
+s = len(list)
 
-
+i = 0
+while i < s:
+  e = list[i]
   
+  if e.find('_') == -1:
+    c = 0
+    j = i + 1
+    if e in list[j:]:
+      while j < s:
+        if list[j] == e:
+          c += 1
+          list[j] = list[i] + '_' + str(c)
+        j += 1
+  i += 1 
 
-
-   
-    
-
-
-
-
-
-
-print(list)
+print(*list)
