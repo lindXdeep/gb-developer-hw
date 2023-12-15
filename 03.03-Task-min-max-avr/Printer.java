5c9b649a5841dc6cc1b468652038ff4ce4381a55
lindxdeep <linder.igor@gmail.com>
2023-12-15 10:49:45 +0300

    Задача 3: Анализатор списка

--- 03.03-Task-min-max-avr/Printer.java
+++ 03.03-Task-min-max-avr/Printer.java
@@ -1,20 +1,27 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.List;
 
 class Answer {
-  public static void removeEvenNumbers(Integer[] arr) {
+  public static void analyzeNumbers(Integer[] arr) {
     // Введите свое решение ниже
-    ArrayList<Integer> list = new ArrayList<>();
+    List<Integer> list = Arrays.asList(arr);
 
-    list = Arrays
-        .stream(arr)
-        .mapToInt(Integer::intValue)
-        .filter(n -> n % 2 != 0)
-        .boxed()
-        .collect(Collectors.toCollection(ArrayList::new));
+    Collections.sort(list);
+
+    int min = list.get(0);
+    int max = list.get(list.size() - 1);
+    double avr = 0;
+
+    for (Integer a : list)
+      avr += a.intValue();
+
+    avr = avr / list.size();
 
     System.out.println(list);
+    System.out.printf("Minimum is %d%n", min);
+    System.out.printf("Maximum is %d%n", max);
+    System.out.println("Average is = " + avr);
   }
 }
 
@@ -25,7 +32,7 @@ public class Printer {
 
     if (args.length == 0) {
       // При отправке кода на Выполнение, вы можете варьировать эти параметры
-      arr = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      arr = new Integer[] { -2, -1, 0, 1, 2, 3, 4, 5 };
     } else {
       arr = Arrays.stream(args[0].split(", "))
           .map(Integer::parseInt)
@@ -33,6 +40,6 @@ public class Printer {
     }
 
     Answer ans = new Answer();
-    ans.removeEvenNumbers(arr);
+    ans.analyzeNumbers(arr);
   }
 }
